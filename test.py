c5e1db2cbaef81d7fafabac254d1c55e02161528
Vignesh <vignesh@machdatum.com>
2021-03-02 10:32:16 +0530

    Initial commit

---
+++ test.py
@@ -0,0 +1,10 @@
+from pyflink.table import DataTypes
+
+from pyflink.table.udf import udf
+
+
+@udf(input_types=[DataTypes.STRING()], result_type=DataTypes.STRING())
+
+def func1(s: str):
+
+   return s.replace('Hello-world', 'Hey!! Hooray!!')
